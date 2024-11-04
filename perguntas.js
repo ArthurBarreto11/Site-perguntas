33603e9c42889e21e2dc4d23a2351f19de81631d
ArthurBarreto11 <132924706+ArthurBarreto11@users.noreply.github.com>
2024-11-04 11:45:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,21 @@
+criaCartao(
+    'Batman',
+    'Qual é o seu nome verdadeiro?',
+    'Bruce Wayne'
+)
+criaCartao(
+    'Batman',
+    'Qual é o maior vilão do Batman',
+    'Coringa'
+    )
+criaCartao(
+    'Batman',
+    'Qual é a sua idade?',
+     '35'
+)
+criaCartao(
+    'Batman',
+    'O que ocasionou o surgimento do Batman?',
+    'O assassinato de seus pais quando ainda era criança'
+)
+           
